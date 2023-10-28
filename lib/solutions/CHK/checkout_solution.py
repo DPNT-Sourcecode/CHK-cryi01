dbfc4e7044e0a6387e9d699c166e07e01a801121
sbx_user1051 <sbx_user1051@169.254.65.93>
2023-10-28 09:30:58 +0000

    Sat Oct 28 09:30:58 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,15 +10,16 @@ def checkout(skus: str) -> int:
         skus (str): a String containing the SKUs of all the products in the basket
     
     Returns:
-        int: an Integer representing the total checkout value of the items 
+        int: an Integer representing the total checkout value of the items
     """
     checkout_total = 0
     
     # parse input string first
     basket_items = [item for item in skus]
-    print(basket_items)
 
     for item in basket_items:
+        if item not in ["A", "B", "C", "D"]:
+            return -1
         if item.strip()  == "A":
             checkout_total += 50
         if item.strip()  == "B":
@@ -29,5 +30,3 @@ def checkout(skus: str) -> int:
             checkout_total += 15
 
     return checkout_total
-
-checkout("ABC")

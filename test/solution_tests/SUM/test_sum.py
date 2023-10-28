b4544c576cfe3f6ec2b907bd48e7606bb43ff270
sbx_user1051 <sbx_user1051@169.254.117.229>
2023-10-28 08:26:36 +0000

    Sat Oct 28 08:26:36 UTC 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -8,14 +8,14 @@ class TestSum():
         assert sum_solution.compute(50, 23) == 73
         assert sum_solution.compute(0, 0) == 0
         assert sum_solution.compute(100, 100) == 200
-	
-    def test_invalid_sum(self):
-        assert sum_solution.compute(-5, 10) == 5
-        assert sum_solution.compute(111, 200) == 311
         
-	def test_raise_errors(self):
+    def test_raise_errors(self):
         with pytest.raises(ValueError):
-            
-		
+            # test usage of non-positive numbers
+            sum_solution.compute(-5, 10)
+            # test out of limits inputs
+            sum_solution.compute(120, 10)
+
+
 
 

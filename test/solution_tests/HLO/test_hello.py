3f75a55d5102fc4a562bb7a0e9d331a5644db49d
sbx_user1051 <sbx_user1051@169.254.117.229>
2023-10-28 08:50:02 +0000

    Sat Oct 28 08:50:02 UTC 2023

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -2,11 +2,12 @@ from solutions.HLO import hello_solution
 import pytest
 
 
-class TestSum():
-    def test_valid_sum(self):
+class TestHello():
+    def test_valid_hello(self):
         assert hello_solution.hello("Craftsman") == "Hello, Craftsman!"
         assert hello_solution.hello("Mr. X") == "Hello, Mr. X!"
         assert hello_solution.hello("John") == "Hello, John!"
+
 
 
 

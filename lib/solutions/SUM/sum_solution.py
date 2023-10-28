911fa4c75e837479330153828c5f9b671f995227
sbx_user1051 <sbx_user1051@169.254.117.229>
2023-10-28 08:20:36 +0000

    Sat Oct 28 08:20:36 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,9 +1,17 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
+    """
+    Description
+
+    Args:
+
+    Returns:
+    """
     
     if 0 <= x <= 100 and 0 <= y <= 100:
         return x + y
     else:
+        raise ValueError("Input parameters must be positive integers 0 and 100")
 
 
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -2,5 +2,12 @@ from solutions.SUM import sum_solution
 
 
 class TestSum():
-    def test_sum(self):
+    def test_valid_sum(self):
         assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(50, 23) == 73
+        
+	
+    def test_invalid_sum(self):
+        assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(50, 23) == 73
+

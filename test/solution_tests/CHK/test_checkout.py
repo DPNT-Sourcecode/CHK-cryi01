13462ced09bea86ae50e8b9fc0c05a74fcd123a5
sbx_user1051 <sbx_user1051@169.254.65.93>
2023-10-28 09:24:58 +0000

    Sat Oct 28 09:24:58 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,10 +4,11 @@ import pytest
 
 class TestCheckout():
     def test_valid_checkout(self):
-        assert checkout_solution.checkout("A, B, C, D") == 115
-        assert checkout_solution.checkout("3A, 2B, C, D") == 210
+        assert checkout_solution.checkout("ABCD") == 115
+        assert checkout_solution.checkout("AAABBCD") == 210
         assert checkout_solution.checkout("D") == 15
         assert checkout_solution.checkout("C") == 20
-        assert checkout_solution.checkout("2B, C, D") == 80
-        assert checkout_solution.checkout("C, D") == 35
+        assert checkout_solution.checkout("BBCD") == 80
+        assert checkout_solution.checkout("CD") == 35
+
 

f543ee1382a339bcf45c0f9eed64d8a24959c259
sbx_user1051 <sbx_user1051@169.254.117.229>
2023-10-28 08:29:36 +0000

    Sat Oct 28 08:29:36 UTC 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,17 +1,21 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
     """
-    Description
+    Adds two positive integers. Input parameters must be between 0 and 100.
 
     Args:
+        x (int): a positive integer between 0-100
+        y (int): a positive integer between 0-100
 
     Returns:
+        int: an Integer representing the sum of the two numbers
     """
     
     if 0 <= x <= 100 and 0 <= y <= 100:
         return x + y
     else:
         raise ValueError("Input parameters must be positive integers 0 and 100")
+
 
 
 

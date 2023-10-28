c562eda32d9093ef9f9135047e5193c375aa7173
sbx_user1051 <sbx_user1051@169.254.65.93>
2023-10-28 09:07:45 +0000

    Sat Oct 28 09:07:45 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,11 +7,20 @@ def checkout(skus: str) -> int:
     Desc
     
     Args:
-		skus (str): a String containing the SKUs of all the products in the basket
+        skus (str): a String containing the SKUs of all the products in the basket
     
     Returns:
-		int: an Integer representing the total checkout value of the items 
+        int: an Integer representing the total checkout value of the items 
     """
-    raise NotImplementedError()
+    checkout_total = 0
+    
+    #parse input string first
+
+    items = skus.split(,)
+
+    if skus == "A":
+        checkout_total += 50
+    
+
 
 

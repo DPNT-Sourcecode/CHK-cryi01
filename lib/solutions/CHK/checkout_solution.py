65c6d7980d73177dd729d99b04bf529aada4f1f2
sbx_user1051 <sbx_user1051@169.254.16.201>
2023-10-29 00:28:35 +0000

    Sun Oct 29 00:28:35 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,7 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from math import floor
 def checkout(skus: str) -> int:
     """
     Method used to calculate the total checkout value based on the products contained in a basket
@@ -12,33 +13,133 @@ def checkout(skus: str) -> int:
     Returns:
         int: an Integer representing the total checkout value of the items
     """
+    # build dict with baseline price
+    item_prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15,
+        'E': 40,
+        'F': 10,
+        'G': 20,
+        'H': 10,
+        'I': 35,
+        'J': 60,
+        'K': 70,
+        'L': 90,
+        'M': 15,
+        'N': 40,
+        'O': 10,
+        'P': 50,
+        'Q': 30,
+        'R': 50,
+        'S': 20,
+        'T': 20,
+        'U': 40,
+        'V': 50,
+        'W': 20,
+        'X': 17,
+        'Y': 20,
+        'Z': 21
+
+    }
+
+    # build dict with special offers (list of tuples in the values)
+    special_offers = {
+        'A': [(5, 200), (3, 130)],
+        'B': [(2, 45)],
+        'H': [(10, 80), (5, 45)],
+        'K': [(2, 120)],
+        'P': [(5, 200)],
+        'Q': [(3, 80)],
+        'V': [(3, 130), (2, 90)],
+    }
+
+    free_offers = {
+        'E': [(2, 'B')],
+        'F': [(3, 'F')],
+        'N': [(3, 'M')],
+        'R': [(3, 'Q')],
+        'U': [(4, 'U')],
+    }
+
+    group_discount_offers = {
+        'E': [(2, 'B')],
+    }
+
+    group_discount_offers_list = ['S','T','X','Y','Z']
+
+
     checkout_total = 0
-    a_occurence = 0
-    b_occurence = 0
+    n_discount_offers_count = 0
     
     # parse input string first
     basket_items = [item for item in skus]
 
+    # count ocurrences of each item
+    item_counts = {}
     for item in basket_items:
-        if item not in ["A", "B", "C", "D"]:
-            return -1
-        if item.strip()  == "A":
-            a_occurence += 1
-            checkout_total += 50
-            if a_occurence == 3:
-                checkout_total -= 150
-                checkout_total += 130
-                a_occurence = 0
-        if item.strip()  == "B":
-            b_occurence += 1
-            checkout_total += 30
-            if b_occurence == 2:
-                checkout_total -= 60
-                checkout_total += 45
-                b_occurence = 0
-        if item.strip()  == "C":
-            checkout_total += 20
-        if item.strip()  == "D":
-            checkout_total += 15
+        if item in item_counts:
+            item_counts[item] += 1
+        else:
+            item_counts[item] = 1
 
+    found_discount_offers_items = {
+        'S':0,
+        'T':0,
+        'X':0,
+        'Y':0,
+        'Z':0
+    }
+
+    # reverse dict
+    item_counts = {v: item_counts[v] for v in sorted(item_counts.keys(), reverse=True)}
+    filtered_total = 0
+    
+
+    for item, count in item_counts.items():
+        #print(checkout_total)
+        # group discount
+        #print(item_counts)
+        print(item)
+        if item in group_discount_offers_list:
+            found_discount_offers_items[item] += 1 * item_counts[item]
+            if sum(found_discount_offers_items.values()) == 3:
+                filtered_item_counts = {key: value for key, value in found_discount_offers_items.items() if value > 0}
+                #print(filtered_item_counts)
+                for key, value in filtered_item_counts.items():   
+                    filtered_total += value * item_prices[key]
+                    item_counts[key] -= value
+                    found_discount_offers_items[key] -= 1
+                    #print(found_discount_offers_items)
+                #print(filtered_item_counts)
+                checkout_total -= filtered_total - item_prices[item]
+                checkout_total += 45
+            #found_discount_offers_items[item] -= 1   
+            print(found_discount_offers_items)
+            
+        #print(item_counts)
+        # free offers
+        if item in free_offers:
+            for free_offer in free_offers[item]:
+                free_offers_count, free_offers_item = free_offer
+                if free_offers_item in item_counts.keys() and item_counts[free_offers_item] > 0:
+                    item_counts[free_offers_item] -= abs(floor(item_counts[item] / free_offers_count))
+        # special offers
+        if item in special_offers:
+            for special_offer in special_offers[item]:
+                special_offer_count, special_offer_price = special_offer
+                while item_counts[item] >= special_offer_count:
+                    checkout_total += special_offer_price
+                    item_counts[item] -= special_offer_count
+        # normal cases
+        if item_counts[item] > 0:
+            if item in item_prices:
+                checkout_total += item_counts[item] * item_prices[item]
+            else:
+                return -1
+    #print(found_discount_offers_items)
+    print(checkout_total)
     return checkout_total
+
+checkout('STXST')
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,8 +11,32 @@ class TestCheckout():
         assert checkout_solution.checkout("BBCD") == 80
         assert checkout_solution.checkout("BBBB") == 90
         assert checkout_solution.checkout("AAABBCD") == 210
-        assert checkout_solution.checkout("AAAAAA") == 260
-        assert checkout_solution.checkout("AAAAA") == 230
+        assert checkout_solution.checkout("AAAAA") == 200
+        assert checkout_solution.checkout("EEB") == 80
+        assert checkout_solution.checkout("EEEEB") == 160
+        assert checkout_solution.checkout("EEEEBB") == 160
+        assert checkout_solution.checkout("EEEB") == 120
+        assert checkout_solution.checkout("AAAAAA") == 250
+        assert checkout_solution.checkout("AAAAAAA") == 300
+        assert checkout_solution.checkout("BEBEEE") == 160
+        assert checkout_solution.checkout("ABCDEABCDE") == 280
+        assert checkout_solution.checkout("ABCDECBAABCABBAAAEEAA") == 665
+        assert checkout_solution.checkout("FFF") == 20
+        assert checkout_solution.checkout("FFFF") == 30
+        assert checkout_solution.checkout("FFFFF") == 40
+        assert checkout_solution.checkout("FFFFFF") == 40
+        assert checkout_solution.checkout("FFFFFFF") == 50
+        assert checkout_solution.checkout("HHHHH") == 45
+        assert checkout_solution.checkout("HHHHHH") == 55
+        assert checkout_solution.checkout("HHHHHHHHHH") == 80
+        assert checkout_solution.checkout("KK") == 150
+        assert checkout_solution.checkout("NNNM") == 120
+        assert checkout_solution.checkout("PPPPP") == 200
+        assert checkout_solution.checkout("QQQ") == 80
+        assert checkout_solution.checkout("RRRQQ") == 180
+        assert checkout_solution.checkout("UUUU") == 120
+        assert checkout_solution.checkout("VVV") == 130
+        assert checkout_solution.checkout("VVVVV") == 220
     
     def test_invalid_checkout(self):
         assert checkout_solution.checkout("-") == -1

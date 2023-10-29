e4d3be8e2cafa24251ba57e6db150e9550f831ee
sbx_user1051 <sbx_user1051@169.254.240.45>
2023-10-29 11:05:39 +0000

    Sun Oct 29 11:05:39 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -103,10 +103,10 @@ def checkout(skus: str) -> int:
                 print("Apply Discount!")
                 filtered_item_counts = {key: value for key, value in found_discount_offers_items.items() if value > 0}
                 for discount_items in filtered_item_counts:
-                    if available_discount > 0:
+                    if available_discount == 3:
                         print("available_discount:",available_discount)
-                    item_counts[discount_items] -= filtered_item_counts[discount_items]
-                    available_discount -= filtered_item_counts[discount_items]
+                        item_counts[discount_items] -= filtered_item_counts[discount_items]
+                        available_discount -= filtered_item_counts[discount_items]
                 discount_item_count = 0
                 #print(filtered_item_counts)
                 #item_counts[discount_item] -= found_discount_offers_items[discount_item]
@@ -151,6 +151,7 @@ def checkout(skus: str) -> int:
     return checkout_total
 
 checkout('STX')
+
 
 
 
